eb18a6a2ed105f73d11fa2f1eb0eb24bdf32fb13
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2022-08-10 11:41:58 +0100

    create user

--- src/@meusgastos/users/domain/domain/entity/user.spec.ts
+++ src/@meusgastos/users/domain/domain/entity/user.spec.ts
@@ -1,5 +1,50 @@
+import { EmailAddress } from "../valueObject/emailAddress";
+import { UserId } from "../valueObject/userId";
+import { UserName } from "../valueObject/userName";
+import { User } from "./user";
+import { v4 as uuid } from "uuid";
+
 describe("User unit test", () => {
-  it("should throw error if Id is empty", () => {
-    expect(2).toBe(2);
+  it("should throw error if Id is invalid", () => {
+    expect(() => {
+      const user = User.create(
+        UserId.fromString(""),
+        UserName.pick("tandavala"),
+        EmailAddress.pick("jose.tandavala@gmail.com")
+      );
+    }).toThrowError("The value does not represent a valid identifier");
+  });
+
+  it("should throw error if UserName is empty", () => {
+    expect(() => {
+      const user = User.create(
+        UserId.fromString(uuid()),
+        UserName.pick(""),
+        EmailAddress.pick("jose.tandavala@gmail.com")
+      );
+    }).toThrowError("Username is required");
+  });
+
+  it("should throw error if email is empty", () => {
+    expect(() => {
+      const user = User.create(
+        UserId.fromString(uuid()),
+        UserName.pick("tandavala"),
+        EmailAddress.pick("")
+      );
+    }).toThrowError("Email is required");
+  });
+
+  it("should create a user", () => {
+    const id = uuid();
+    const user = User.create(
+      UserId.fromUuid(id),
+      UserName.pick("tandavala"),
+      EmailAddress.pick("jose.tandavala@gmail.com")
+    );
+
+    expect(user.getId()).toBe(id);
+    expect(user.getUserName()).toBe("tandavala");
+    expect(user.getEmailAddress()).toBe("jose.tandavala@gmail.com");
   });
 });
--- src/@meusgastos/users/domain/domain/entity/user.ts
+++ src/@meusgastos/users/domain/domain/entity/user.ts
@@ -1,7 +1,61 @@
+import verify from "../../../../@shared/domain/verification/verify";
 import { TriggerEvents } from "../../../../@shared/infrastructure/event/triggerEvent";
+import { EmailAddress } from "../valueObject/emailAddress";
+import { UserId } from "../valueObject/userId";
+import { UserName } from "../valueObject/userName";
 
 export class User extends TriggerEvents {
-  constructor() {
+  private _uuid: string;
+  public _userName: string;
+  public _email: string;
+
+  constructor(uuid: string, userName: string, email: string) {
     super();
+    verify("Id", uuid);
+    verify("Username", userName);
+    verify("Email", email);
+
+    this._uuid = uuid;
+    this._userName = userName;
+    this._email = email;
+  }
+
+  public static create(
+    uuid: UserId,
+    userName: UserName,
+    emailAddress: EmailAddress
+  ) {
+    return new User(
+      uuid.toString(),
+      userName.__toString(),
+      emailAddress.__toString()
+    );
+  }
+
+  public getId() {
+    return this._uuid;
+  }
+
+  public setId(uuid: string): UserId {
+    verify("Id", uuid);
+    return UserId.fromString(uuid);
+  }
+
+  public setUserName(userName: string): UserName {
+    verify("Username", userName);
+    return UserName.pick(userName);
+  }
+
+  public getUserName() {
+    return this._userName;
+  }
+
+  public setEmailAddress(emailAddress: string): EmailAddress {
+    verify("Email", emailAddress);
+    return EmailAddress.pick(emailAddress);
+  }
+
+  public getEmailAddress() {
+    return this._email;
   }
 }
